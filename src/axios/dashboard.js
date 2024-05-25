510fc70d211754a35510270bd8f2f45b845657a2
JANGA GURU PAVANI <Pavani.Janga@iiitb.ac.in>
2024-05-25 08:31:49 +0530

    dashboard api bug fix

--- src/axios/dashboard.js
+++ src/axios/dashboard.js
@@ -3,7 +3,9 @@ import axios from "./instance";
 
 export const getDashboardInfo = async (data) => {
   try {
-    const res = await axios.get("/api/dashboard/getDashboardDetails", data);
+    const res = await axios.get("/api/dashboard/getDashboardDetails", {
+      params: data,
+    });
     return res.data;
   } catch (error) {
     return handleErrResponse(error.response);
